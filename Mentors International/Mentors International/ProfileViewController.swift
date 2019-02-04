32c4bfcfdeebe8c9f026c31dacf2ad60b8616bbf
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-02-04 14:24:59 -0800

    UI and File Skeletons

---
+++ Mentors International/Mentors International/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Mentors International
+//
+//  Created by Jaspal on 2/4/19.
+//  Copyright © 2019 Jaspal Suri. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
